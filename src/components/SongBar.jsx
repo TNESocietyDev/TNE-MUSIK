a6058eb033e1ebc561bc2727168f6b6f87f5f995
TNESocietyDev <augustine4717@gmail.com>
2024-03-19 12:19:24 +0100

    update

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -10,7 +10,8 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
-        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart}
+        src={artistId ? song.image : ""}
+        // artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart
         alt={song?.title}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
--- src/mockTopCharts.js
+++ src/mockTopCharts.js
@@ -1,17 +1,24 @@
 // mockTopCharts.js
 export const mockTopCharts = [
-    {
-      key: '1',
-      title: 'Song 1',
-      artist: 'Artist 1',
-      // ... other song properties
-    },
-    {
-      key: '2',
-      title: 'Song 2',
-      artist: 'Artist 2',
-      // ... other song properties
-    },
-    // Add more sample top charts as needed
+  {
+    key: '3',
+    title: 'Song 3',
+    artist: 'Artist 3',
+    genre: 'HIP_HOP_RAP',
+    audioFilePath: 'audio/3.mp3',
+    image: "images/img3.jpg",
+    subtitle: "song 3",
+    // ... other song properties
+  },
+  {
+    key: '4',
+    title: 'Song 4',
+    artist: 'Artist 4',
+    genre: 'POP',
+    audioFilePath: 'audio/4.mp3', // Corrected from 'udioFilePath' to 'audioFilePath'
+    image: "images/img4.jpg",
+    subtitle: "song 4",
+    // ... other song properties
+  },
+  // Add more sample top charts as needed
 ];
-  
